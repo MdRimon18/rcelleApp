086d1cb1fd879745944d150022099514f583f94d
rimon19 <mominur.hossain@outlook.com>
2024-01-27 23:24:54 +0600

    first commit

---
+++ rCellE/rCellE/wwwroot/assets/js/custom.js
@@ -0,0 +1,17 @@
+﻿function design_resolver_script() {
+    var isRTL = JSON.parse(localStorage.getItem('isRTL'));
+    if (isRTL) {
+        call('if');
+        var linkDefault = document.getElementById('style-default');
+        var userLinkDefault = document.getElementById('user-style-default');
+        linkDefault.setAttribute('disabled', true);
+        userLinkDefault.setAttribute('disabled', true);
+        document.querySelector('html').setAttribute('dir', 'rtl');
+    } else {
+        alert('call else');
+        var linkRTL = document.getElementById('style-rtl');
+        var userLinkRTL = document.getElementById('user-style-rtl');
+        linkRTL.setAttribute('disabled', true);
+        userLinkRTL.setAttribute('disabled', true);
+    }
+}
